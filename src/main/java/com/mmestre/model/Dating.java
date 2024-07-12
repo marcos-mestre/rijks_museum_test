a05ca411aaa40ccdfb5d3bf018597b7b71430eba
Marcos Mestre <mmestre@gmail.com>
2024-07-12 20:23:34 +0200

    Complete missing details fields Added some missing fields in the java beans which stores the json response from the Collection details response.

--- src/main/java/com/mmestre/model/Dating.java
+++ src/main/java/com/mmestre/model/Dating.java
@@ -1,4 +1,10 @@
 package com.mmestre.model;
 
+@lombok.Getter @lombok.Setter
 public class Dating {
+    private String presentingDate;
+    private long sortingDate;
+    private long period;
+    private long yearEarly;
+    private long yearLate;
 }
--- src/main/java/com/mmestre/model/Dimension.java
+++ src/main/java/com/mmestre/model/Dimension.java
@@ -1,4 +1,10 @@
 package com.mmestre.model;
 
+@lombok.Getter @lombok.Setter
 public class Dimension {
+    private String unit;
+    private String type;
+    private String precision;
+    private String part;
+    private String value;
 }
