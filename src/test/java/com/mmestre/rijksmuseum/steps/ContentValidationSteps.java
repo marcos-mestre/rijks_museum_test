087b516219ce727d781f5683fc2bbc8e64948d5c
Marcos Mestre <marcos-mestre@users.noreply.github.com>
2024-07-18 19:37:22 +0200

    Add proper logging (#24)
    * add_proper_logging
    * pom.xml: added log4j dependency.
    * log4j2.properties: added basic standard log configuration.
    * ContentValidationSteps: replaced existing console printing for logging.
    * Collection.feature: renamed scenarios names to improve readability.
    
    * add_proper_logging
    * ContentValidationSteps: rephrased logging to improve readability.
    * log4j2.properties: added line number to the format.
    
    * add_proper_logging
    * ContentValidationSteps, StatusValidationSteps: add logging to the validation steps.

--- src/test/java/com/mmestre/rijksmuseum/steps/ContentValidationSteps.java
+++ src/test/java/com/mmestre/rijksmuseum/steps/ContentValidationSteps.java
@@ -8,25 +8,31 @@ import com.mmestre.rijksmuseum.model.common.ArtObject;
 import com.mmestre.rijksmuseum.model.details.CollectionDetailsResponse;
 import com.mmestre.rijksmuseum.model.image.CollectionImageResponse;
 import io.cucumber.java.en.Then;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Log4j2
 public class ContentValidationSteps {
     @Then("all the artObjects received in the Collection Response must be made by {string}")
     public void artObjectsMatchesMaker(String expectedMaker) {
         CollectionResponse collectionResponse = CollectionClient.getResponseAsBean();
         assertNotEquals(0, collectionResponse.getArtObjects().size(),
                 "There should be at least one art object to compare.");
-        List<Executable> assertions = collectionResponse.getArtObjects().stream()
-                .<Executable>map(artObject -> () -> assertEquals(expectedMaker, artObject.getPrincipalOrFirstMaker(),
-                        artObject.getId() + ": For this object the maker didn't match.")).collect(Collectors.toList());
+        List<Executable> assertions = new ArrayList<>();
+        for (ArtObject artObject : collectionResponse.getArtObjects()) {
+            log.debug("Object ID [{}]: Object principalOrFirstMaker: {}, expected maker: {}.",
+                    artObject.getId(), artObject.getPrincipalOrFirstMaker(), expectedMaker);
+            Executable executable = () -> assertEquals(expectedMaker, artObject.getPrincipalOrFirstMaker(),
+                    artObject.getId() + ": For this object the maker didn't match.");
+            assertions.add(executable);
+        }
         assertAll("Not all the art objects matches the expected maker.", assertions);
     }
 
@@ -34,7 +40,10 @@ public class ContentValidationSteps {
     public void artObjectMatchesId(String expectedId) {
         CollectionDetailsResponse collectionDetailsResponse = CollectionDetailsClient.getResponseAsBean();
         assertNotNull(collectionDetailsResponse.getArtObject(), "The response didn't include any art object.");
-        assertEquals(expectedId, collectionDetailsResponse.getArtObject().getObjectNumber(), "The art object number doesn't match.");
+        String receivedObjectNumber = collectionDetailsResponse.getArtObject().getObjectNumber();
+        log.debug("Received object number '{}', expected '{}‘.", receivedObjectNumber, expectedId);
+        assertEquals(expectedId, collectionDetailsResponse.getArtObject().getObjectNumber(),
+                "The art object number doesn't match.");
     }
 
     @Then("I receive a collection image response with at least {int} image(s)")
@@ -43,6 +52,8 @@ public class ContentValidationSteps {
         assertNotNull(collectionImageResponse, "The response couldn't been parsed to the CollectionImageResponse class.");
         assertNotNull(collectionImageResponse.getLevels(), "Then response didn't return a list of levels");
         int imagesReceived = collectionImageResponse.getLevels().size();
+        log.debug("The collection image returned {} records, with {} expected as minimum.",
+                imagesReceived, minimumImages);
         assertTrue(imagesReceived >= minimumImages, String.format("The images received (%s) should be at least %s.", imagesReceived, minimumImages));
     }
 
@@ -65,7 +76,7 @@ public class ContentValidationSteps {
             throw new UnsupportedOperationException(String.format("The sort method %s has not been implemented yet.", expectedSortMethod));
         }
         for (int i = 0; i < retrievedListOfSortingDate.size(); i++) {
-            System.out.printf("ObjectId: %s, retrieved date %s, sorted date: %s",
+            log.debug("ObjectId: {}, retrieved date {}, framework sorted date: {}",
                     retrievedListOfObjectNumbers.get(i), retrievedListOfSortingDate.get(i), sortedList.get(i));
             assertEquals(retrievedListOfSortingDate.get(i).longValue(),sortedList.get(i).longValue(), "The sorted values are not matching.");
         }
--- src/test/java/com/mmestre/rijksmuseum/steps/StatusValidationSteps.java
+++ src/test/java/com/mmestre/rijksmuseum/steps/StatusValidationSteps.java
@@ -2,11 +2,15 @@ package com.mmestre.rijksmuseum.steps;
 
 import com.mmestre.rijksmuseum.client.CollectionClient;
 import io.cucumber.java.en.Then;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class StatusValidationSteps {
 
     @Then("the Collection API must return status {int}")
-    public void checkCollectionStatus(int expectedStatus) {
-        CollectionClient.getResponse().then().statusCode(expectedStatus);
+    public void checkCollectionStatus(int expectedStatusCode) {
+        int currentStatusCode = CollectionClient.getResponse().getStatusCode();
+        log.debug("Response status code: {}, expected status code: {}.", currentStatusCode, expectedStatusCode);
+        CollectionClient.getResponse().then().statusCode(expectedStatusCode);
     }
 }
