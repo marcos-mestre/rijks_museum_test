0eceea72f1cf9676563cba8a39c1fa8641adfb23
Marcos Mestre <marcos-mestre@users.noreply.github.com>
2024-07-26 07:16:26 +0200

    move_key (#25)
    Remove the hardcoded api key
    * RestClient: retrieves the key from the system properties.
    * mr_maven_verify.yml: read the api key from the env secrets

--- src/main/java/com/mmestre/rijksmuseum/client/RestClient.java
+++ src/main/java/com/mmestre/rijksmuseum/client/RestClient.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 class RestClient {
-    protected static final String key = "14plFUIH";
+    protected static final String key = System.getProperty("api.key");
     private static final String baseUrl = "https://www.rijksmuseum.nl";
     private static volatile boolean initiated;
 
